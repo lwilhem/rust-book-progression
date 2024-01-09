220299fb18c1f42de4f41216fbc7e87846216d17
lwilhem <wilhem.lecanu@edu.devinci.fr>
2024-01-09 21:05:56 +0100

    docs: control flow fibonnacci

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -3,6 +3,7 @@ use std::io;
 fn main() {
   println!("Hello, world!");
   fahrenheit_to_celsius();
+  fibonnaci();
 }
 
 fn fahrenheit_to_celsius() {
@@ -24,5 +25,42 @@ fn fahrenheit_to_celsius() {
 
   let celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
 
-  println!("{celsius}")
+  println!("{celsius}");
+}
+
+fn fibonnaci() -> i32 {
+  let mut last_term = String::new();
+
+  println!("input N term");
+
+  io::stdin()
+    .read_line(&mut last_term)
+    .expect("Only use integer 32 positive value");
+
+  let last_term: i32 = match last_term.trim().parse() {
+    Ok(num) => num,
+    Err(_) => {
+      println!("err");
+      return 0;
+    }
+  };
+
+  match last_term {
+    0 => 0,
+    1 => 1,
+    _ => {
+      let mut prev = 0;
+      let mut current = 1;
+
+      for _ in 2..=last_term {
+        let new_value = prev + current;
+        prev = current;
+        current = new_value;
+      }
+
+      println!("{current}");
+
+      current
+    }
+  }
 }
